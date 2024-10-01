8ed020f4dfee17326b4555a04be31442fe213ef4
lola161385 <lola161385@naver.com>
2024-10-01 21:58:30 +0900

    깃허브 연습

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace githubtest
         static void Main()
         {
             // 깃허브 학습
+            // 추가
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
